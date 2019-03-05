9e3482e5e4aa96d1e652cb327490a05973fa324e
earth06 <nnnkjktkt119@gmail.com>
2019-03-05 20:41:06 +0900

    add argument to draw ticklabel or not
    add new argument left and bottom to geogrid function

--- carto_kit.py
+++ carto_kit.py
@@ -7,10 +7,12 @@ import cartopy.feature as cfeature
 import numpy as np
 import matplotlib.ticker as mticker
 import matplotlib.path as mpath
+import matplotlib.pyplot as plt
 
 def set_geogrid(ax,resolution='110m'
                ,dlon=60,dlat=30
                ,manual_ticks=False,xticks=None,yticks=None
+               ,bottom=True,left=True,right=False,top=False
                ,linewidth=0.5,fontsize=15,labelsize=15
                ,color='grey' ,alpha=0.8,linestyle='-' ):
     """
@@ -21,10 +23,21 @@ def set_geogrid(ax,resolution='110m'
     dlat      :float  grid interval of latitude
     linewidth,fontsize,labelsize,alpha :float
     color     :string
+    resolution :string '10m','50m','110m'
+    bottom    :boolean draw  xaxis ticklabel
+    lfet      :boolean draw  yaxis ticklabel
+    
     return 
     -------------
     ax
     """
+#    labelpos=[bottom,left,top,right]
+#        
+#    plt.rcParams['ytick.left']=plt.rcParams['ytick.labelleft']=left
+#    plt.rcParams['ytick.right']=plt.rcParams['ytick.labelright']=right
+#    plt.rcParams['xtick.top']=plt.rcParams['xtick.labeltop']=top
+#    plt.rcParams['xtick.bottom']=plt.rcParams['xtick.labelbottom']=bottom
+
     ax.coastlines(resolution=resolution)
     gl = ax.gridlines(crs=ccrs.PlateCarree()
                       , draw_labels=False,
@@ -44,6 +57,10 @@ def set_geogrid(ax,resolution='110m'
         lonfmt=LongitudeFormatter(zero_direction_label=True)
         ax.xaxis.set_major_formatter(lonfmt)
         ax.yaxis.set_major_formatter(latfmt)
+        if (bottom==False):
+            ax.xaxis.set_major_formatter(plt.NullFormatter())
+        if (left==False):
+            ax.yaxis.set_major_formatter(plt.NullFormatter())
         ax.axes.tick_params(labelsize=labelsize)
     return ax
 def set_feature(ax,scale='110m'):
@@ -52,6 +69,7 @@ def set_feature(ax,scale='110m'):
     parameter
     -----------
     ax    :cartopy.mpl.geoaxes
+    scale :string  '10m,50m or 110m'
     return
     ----------
     ax    :as above
